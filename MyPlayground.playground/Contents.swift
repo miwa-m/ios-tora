b90cebb42ee8d4877c07a41dedba2c540c7db75f
miwa-tora <komura.j3@gmail.com>
2018-04-29 23:18:59 +0900

    edit padding

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,5 @@
 //: Playground - noun: a place where people can play
-
+import Foundation
 
 func add_result_info(_ info_list: [(name: String, point: Int)]) -> [[String:String]]{
     // タプルのリストをName,Point,Resultのキーを持つ辞書のリストにして返す
@@ -15,13 +15,12 @@ func add_result_info(_ info_list: [(name: String, point: Int)]) -> [[String:Stri
 var received_list = [("Alex", 81), ("Ben", 78), ("Clark", 80), ("Donald", 93), ("Edwin", 79)]
 
 let result_list = add_result_info(received_list)
-//print("Name   Point  Result")
-//print("------ -----  -------")
+print("Name   Point  Result")
+print("------ -----  -------")
 // 出力
 for row in result_list{
-    print(row["Name"]! + "   " + row["Point"]! + " " + row["Result"]!)
-    //print(String(format: "%5@", row["Name"]))
-    // ↑列ごとに位置を揃えるためにフォーマットしたいがエラー(argument labels '(_:)' do not match any vailable)
-    // intの変換 String(format: "%05", 1) も同様のエラー
+    // print(row["Name"]! + "   " + row["Point"]! + " " + row["Result"]!)
+    print(row["Name"]!.padding(toLength: 6, withPad: " ", startingAt: 0),
+          row["Point"]!.padding(toLength: 6, withPad: " ", startingAt: 0),
+          row["Result"]!.padding(toLength: 4, withPad: " ", startingAt: 0))
 }
-
